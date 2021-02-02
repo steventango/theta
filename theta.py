9f92b2b17311e83671639de14cfaf59b55cade34
Steven Tang <steventango@users.noreply.github.com>
2021-02-01 19:11:00 -0700

    Rewrite to use class

--- theta.py
+++ theta.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -5,60 +6,81 @@ import subprocess
 import time
 from tqdm import tqdm
 
+
+class theta:
+    def compile(self) -> None:
+        self.executable = self.filename.rreplace('.cpp', '.exe')
+        subprocess.check_output(
+            ['g++', '-o', self.executable, self.filename])
+
+    def time(self) -> None:
+        for i in tqdm(range(2, 200001, 10000)):
+            p = subprocess.Popen(
+                [self.filename],
+                stdout=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+                stderr=subprocess.STDOUT)
+
+            cin = f'{i} 2000000000\n'
+            cin += ' '.join([str(x) for x in range(1000000000,
+                                                   2000000000, 2000000000 // i)])
+
+            start = time.time()
+            p.communicate(input=bytes(cin, 'utf-8'))[0].decode('utf-8')
+            end = time.time()
+            delta = end - start
+            delta *= 1000000
+            self.x.append(i)
+            self.y.append(delta)
+
+    def plot(self) -> None:
+        fig, ax = plt.subplots()
+        ax.ticklabel_format(useOffset=False, style='plain')
+        ax.set_xlabel('n')
+        ax.set_ylabel('time (μs)')
+
+        plt.grid(True)
+
+        m, b = np.polyfit(self.x, self.y, 1)
+        yp = np.polyval([m, b], self.x)
+        plt.plot(self.x, yp, label=f'O(n) [{m:.2f}x + {b:.2f}]')
+
+        m, b = np.polyfit(np.log2(self.x), self.y, 1)
+        yp = np.polyval([m, b], np.log2(self.x))
+        plt.plot(self.x, yp, label=f'O(nlogn) [{m:.2f}log(x) + {b:.2f}]')
+
+        a, b, c = np.polyfit(self.x, self.y, 2)
+        yp = np.polyval([a, b, c], self.x)
+        plt.plot(self.x, yp, label=f'O(n²) [{a:.2f}x² + {b:.2f}x + {c:.2f}]')
+
+        plt.axvline(x=200000)
+        plt.scatter(x, y)
+        ax.set_ylim(0)
+        plt.legend()
+        plt.show()
+
+    def run(self) -> None:
+        if self.filename.endswith('.cpp'):
+            self.compile()
+
+        self.time()
+        self.plot()
+
+        if self.filename.endswith('.cpp'):
+            os.remove(self.executable)
+
+    def __init__(self, filename) -> None:
+        self.filename = filename
+        self.executable = None
+        self.x = []
+        self.y = []
+        pass
+
+
 def main():
-  executable = 'pair_hunt.exe'
+    cpp = theta('pair_hunt.cpp')
+    cpp.run()
 
-  subprocess.check_output(
-      ['g++', '-o', executable, executable.replace('.exe', '.cpp')])
-
-  x = []
-  y = []
-
-  for i in tqdm(range(2, 200001, 10000)):
-    p = subprocess.Popen(
-        [executable],
-        stdout=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-
-    cin = f'{i} 2000000000\n'
-    cin += ' '.join([str(x)
-                    for x in range(1000000000, 2000000000, 2000000000 // i)])
-
-    start = time.time()
-    cout = p.communicate(input=bytes(cin, 'utf-8'))[0].decode('utf-8')
-    end = time.time()
-    delta = end - start
-    delta *= 1000000
-    x.append(i)
-    y.append(delta)
-
-  fig, ax = plt.subplots()
-  ax.ticklabel_format(useOffset=False, style='plain')
-  ax.set_xlabel('n')
-  ax.set_ylabel('time (μs)')
-
-  plt.grid(True)
-
-  m, b = np.polyfit(x, y, 1)
-  yp = np.polyval([m, b], x)
-  plt.plot(x, yp, label=f'O(n) [{m:.2f}x + {b:.2f}]')
-
-  m, b = np.polyfit(np.log2(x), y, 1)
-  yp = np.polyval([m, b], np.log2(x))
-  plt.plot(x, yp, label=f'O(nlogn) [{m:.2f}log(x) + {b:.2f}]')
-
-  a, b, c = np.polyfit(x, y, 2)
-  yp = np.polyval([a, b, c], x)
-  plt.plot(x, yp, label=f'O(n²) [{a:.2f}x² + {b:.2f}x + {c:.2f}]')
-
-  plt.axvline(x=200000)
-  plt.scatter(x, y)
-  ax.set_ylim(0)
-  plt.legend()
-  plt.show()
-
-  os.remove(executable)
 
 if __name__ == '__main__':
-  main()
+    main()
